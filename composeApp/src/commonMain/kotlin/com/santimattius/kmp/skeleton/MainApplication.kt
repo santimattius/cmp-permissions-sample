b6b05cc72c5598a6af8d4890eac0542ca4fa111d
Santiago Mattiauda <santimattius@gmail.com>
2024-06-20 22:19:58 -0300

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/santimattius/kmp/skeleton/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.santimattius.kmp.skeleton
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.navigator.Navigator
+import com.santimattius.kmp.skeleton.di.applicationModules
+import com.santimattius.kmp.skeleton.features.splash.SplashScreen
+import org.koin.compose.KoinApplication
+
+@Composable
+fun MainApplication() {
+    KoinApplication(application = {
+        modules(applicationModules() )
+    }) {
+        Navigator(SplashScreen)
+    }
+}
